4a3b3724678edf89a6353bacd148b32126f2877b
tabaarianna <74322375+tabaarianna@users.noreply.github.com>
2021-03-19 00:18:31 -0700

    Add files via upload

---
+++ Script/PlayerSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour 
+{
+	SOActorModel actorModel;
+	GameObject playerShip;
+	
+	void Start()
+	{
+		CreatePlayer();
+	}
+	
+	void CreatePlayer()
+	{
+		//CREATE PLAYER
+        actorModel = Object.Instantiate(Resources.Load("Script/ScriptableObject/Player_Default")) as SOActorModel;
+        playerShip = GameObject.Instantiate(actorModel.actor) as GameObject;
+        playerShip.GetComponent<Player>().ActorStats(actorModel);
+
+		//SET PLAYER UP
+		playerShip.transform.rotation = Quaternion.Euler(-90,180,0);
+		playerShip.transform.localScale = new Vector3(60,60,60);
+		playerShip.GetComponentInChildren<ParticleSystem>().transform.localScale = new Vector3(25, 25, 25);
+		playerShip.name = "Player";
+		playerShip.transform.SetParent(this.transform); 
+		playerShip.transform.position = Vector3.zero;
+	}
+}
